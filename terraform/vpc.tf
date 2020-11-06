1621859f92a3b4d8be79e7ca51efbb6deb8bb8dd
Arthur Dayton <arthur.dayton@slalom.com>
2020-11-06 07:53:25 -0800

    add subnet

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -22,3 +22,13 @@ resource "aws_vpc" "main" {
     Name = "sample-company-vpc"
   }
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "sample-company-vpc-subnet-private"
+    Access = "private"
+  }
+}
