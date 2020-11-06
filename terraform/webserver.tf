8a808e160e907ba1824441c85da2fe927f42b2f3
Arthur Dayton <arthur.dayton@slalom.com>
2020-11-06 11:54:45 -0800

    Update webserver.tf

--- terraform/webserver.tf
+++ terraform/webserver.tf
@@ -1,23 +1,23 @@
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amazon/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["amazon"] # Canonical
-}
+//data "aws_ami" "ubuntu" {
+//  most_recent = true
+//
+//  filter {
+//    name   = "name"
+//    values = ["amazon/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+//  }
+//
+//  filter {
+//    name   = "virtualization-type"
+//    values = ["hvm"]
+//  }
+//
+//  owners = ["amazon"] # Canonical
+//}
 
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-07a29e5e945228fa1"
   instance_type = "t3.micro"
   subnet_id = aws_subnet.i_public.id
 
