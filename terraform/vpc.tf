642d454f4c712abd2fafc07539fff89d1bad9cff
Arthur Dayton <arthur.dayton@slalom.com>
2020-11-06 08:19:04 -0800

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -23,7 +23,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "i_private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = false
@@ -34,7 +34,7 @@ resource "aws_subnet" "main" {
   }
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "i_public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
   map_public_ip_on_launch = true
