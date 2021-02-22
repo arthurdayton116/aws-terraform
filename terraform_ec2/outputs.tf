e822cdbdeb6de9382540893238c5e602c41ef24e
Arthur Dayton <arthur@arthurdayton.com>
2021-02-21 19:54:05 -0800

    add outputs

--- terraform_ec2/outputs.tf
+++ terraform_ec2/outputs.tf
@@ -13,3 +13,15 @@ output "webserver_ip" {
 output "webserver_link" {
   value = "http://${aws_instance.web.public_ip}"
 }
+
+output "public_ssh_link" {
+  value = "ssh -i ~/.ssh/id_rsa_ec2 ubuntu@${aws_instance.web.public_ip}"
+}
+
+output "copy_private_key_to_ec2" {
+  value = "scp -i ~/.ssh/id_rsa_ec2 ~/.ssh/id_rsa_ec2 ubuntu@${aws_instance.web.public_ip}:~/.ssh/id_rsa_ec2"
+}
+
+output "private_ssh_link" {
+  value = "ssh -i ~/.ssh/id_rsa_ec2 ubuntu@${aws_instance.web2.private_ip}"
+}
