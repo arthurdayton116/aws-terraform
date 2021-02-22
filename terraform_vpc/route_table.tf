0b645f1d87acdd5880aaf57a5bdab6ee039903fa
Arthur Dayton <arthur@arthurdayton.com>
2021-02-21 22:48:01 -0800

    add nat gateway

--- terraform_vpc/route_table.tf
+++ terraform_vpc/route_table.tf
@@ -15,7 +15,7 @@ resource "aws_default_route_table" "main" {
 }
 
 ## Create route table
-resource "aws_route_table" "rtb_public" {
+resource "aws_route_table" "i_public" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
@@ -30,12 +30,12 @@ resource "aws_route_table" "rtb_public" {
 }
 
 ## Associate route tables with subnets
-resource "aws_route_table_association" "rta_subnet_private" {
+resource "aws_route_table_association" "i_subnet_private" {
   subnet_id      = aws_subnet.subnet_private.id
   route_table_id = aws_default_route_table.main.id
 }
 
-resource "aws_route_table_association" "rta_subnet_public" {
+resource "aws_route_table_association" "i_subnet_public" {
   subnet_id      = aws_subnet.subnet_public.id
-  route_table_id = aws_route_table.rtb_public.id
+  route_table_id = aws_route_table.i_public.id
 }
